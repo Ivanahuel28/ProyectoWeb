23fba6c632f16a341ff53f93bc989a3fc9ffeecc
Ivan <ivanahuel.ing@gmail.com>
2021-02-01 20:06:15 -0300

    Finalización app Servicios

--- ProyectoWeb/urls.py
+++ ProyectoWeb/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('servicios/', include('servicios.urls')),
+
     path('',include('ProyectoWebApp.urls')),
 ]
 
--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -2,20 +2,12 @@ from django.shortcuts import render, HttpResponse
 from django.conf import settings
 from django.http import HttpResponse
 
-from servicios.models import Servicio
-
 
 # Create your views here.
 
 def home(request):
 
     return render(request,"ProyectoWebApp/home.html")
-
-def servicios(request):
-
-    servicios=Servicio.objects.all()
-
-    return render(request,"ProyectoWebApp/servicios.html", {"servicios":servicios})
 
 def tienda(request):
 
